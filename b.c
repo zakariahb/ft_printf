edf83570fccdff8eb6e3992895769ba024a2a088
Zakaria Laksyar <zalaksya@e2r6p3.1337.ma>
2024-11-25 11:40:50 +0100

    efw

--- b.c
+++
@@ -1,6 +0,0 @@
-#include "libc.h"
-int main ()
-{
-    printf("%x\n",255);
-    printf("%X\n",255);
-}
--- main.c
+++
@@ -1,9 +0,0 @@
-#include "libc.h"
-#include "ft_printf.h"
-
-int main ()
-{
-    printf("heloo%s\n","eiuw");
-    printf("\n");
-    ft_printf("heloo%s\n","eiuw");
-}
