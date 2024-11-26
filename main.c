a886c8ff503d65f7cc80ce16bba5176a8a100eaf
Zakaria Laksyar <zalaksya@e3r6p4.1337.ma>
2024-11-26 19:23:17 +0100

    hello

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-#include "libc.h"
-int main() {
-	int i = 9;
-	int *p = &i;
-    ft_printf("%p\n", p);  // Outputs: 42 hello 100
-    printf("%p\n",p);  // Outputs: 42 hello 100
-    return 0;
-}
